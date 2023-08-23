50dfff2a03613f02cccb6ad4a924a5ba1c4850fa
Harim Seong <hseong@student.42seoul.kr>
2023-08-23 22:32:40 +0900

    todo: mariadb config for remote connection

--- srcs/requirements/mariadb/tools/mariadb_entrypoint.sh
+++ srcs/requirements/mariadb/tools/mariadb_entrypoint.sh
@@ -1,3 +1,21 @@
 #!/bin/sh
 
-exec bash
+# start temporary server
+mysqld&
+service mysql status
+while [ $? = 3 ]; do sleep 1; service mysql status; done
+
+mysql <<EOF
+UPDATE mysql.user SET Password=PASSWORD('mysql') WHERE User='root';
+DELETE FROM mysql.user WHERE User='';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+DROP DATABASE IF EXISTS test;
+GRANT ALL PRIVILEGES ON *.* TO 'mysql'@'wordpress' IDENTIFIED BY 'mysql' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+EOF
+
+# stop temporary server
+kill $!
+wait $!
+
+exec mysqld_safe --user=mysql --bind-address=0.0.0.0
