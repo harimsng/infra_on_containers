fee5470a85b6966a0722917b71894b55393f80e3
Harim Seong <hseong@student.42seoul.kr>
2023-08-25 14:09:03 +0900

    todo: php config

--- srcs/requirements/wordpress/tools/wordpress_entrypoint.sh
+++ srcs/requirements/wordpress/tools/wordpress_entrypoint.sh
@@ -1,5 +1,5 @@
-php wp-cli.phar core download
-php wp-cli.phar config create --dbuser=mysql --dbpass=mysql --dbname=wp --dbhost=mariadb --skip-check
-php wp-cli.phar core install --url=hseong.42.fr --title=hseong --admin_user=halim --admin_email=halim9512@naver.com
+php wp-cli.phar core download --path=/home/wp/data
+php wp-cli.phar config create --dbuser=mysql --dbpass=mysql --dbname=wp --dbhost=mariadb --url=https://localhost:8000/ --skip-check --path=/home/wp/data
+#php wp-cli.phar core install --url=hseong.42.fr --title=hseong --admin_user=halim --admin_email=halim9512@naver.com
 
 exec bash
